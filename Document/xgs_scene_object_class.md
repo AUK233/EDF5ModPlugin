1412682492d9610081e5f38f96088a4938133e69
zhouGH <95056714+AUK233@users.noreply.github.com>
2023-01-24 14:50:17 +0800

    giant ant
    Only these features can be added now, the most important AmmoClass can not be implemented.

--- Document/xgs_scene_object_class.md
+++ Document/xgs_scene_object_class.md
@@ -7,7 +7,10 @@ Added the following values available for SGO node names.
 ### GiantAnt
 | sgo node name | value type | function | value description |
 |---|---|---|---|
-|ant_BulletColor|ptr|change ant acid color|[color Parameters](#GiantAnt-BulletColor), all parameters must be present|
+|ant_BulletAlive|int|change acid presence time|bulletType = 0, default 180; = 1, 240|
+|ant_BulletColor|ptr|change ant acid color|[color parameters](#GiantAnt-BulletColor), all parameters must be present|
+|ant_BulletExSet|ptr|change ant acid detail|[exset parameters](#GiantAnt-BulletExSet), all parameters must be present|
+|ant_BulletFireHeight|float|change acid launch height||
 
 #### GiantAnt BulletColor
 | node index | value type | value description |
@@ -16,5 +19,17 @@ Added the following values available for SGO node names.
 |1|float|green channel|
 |2|float|blue channel|
 |3|float|alpha channel|
+
+#### GiantAnt BulletExSet
+| node index | value type | value description |
+|---|---|---|
+|0|int|multiply this factor by original shot count, this must > 0|
+|1|float|multiply this factor by original acid speed|
+|2|float|multiply this factor by original acid damage|
+|3|float|change acid explosion range|
+|4|int|set acid penetration, only 0 and 1|
+|5|float|change acid size|
+|6|float|change acid hitbox size|
+|7|float|change acid hit impulse, maybe useless|
 
 ##### [Return to Index](#index)
--- EDFModLoader/ASMAntAcidColor.asm
+++ EDFModLoader/ASMAntAcidColor.asm
@@ -1,9 +1,14 @@
 .data
 extern edf5BDF30Address : qword
 
+; L"ant_BulletAlive"
+antBulletAlive db 97,0,110,0,116,0,95,0,66,0,117,0,108,0,108,0,101,0,116,0,65,0,108,0,105,0,118,0,101,0,0,0
 ; L"ant_BulletColor"
-color db 97,0,110,0,116,0,95,0,66,0,117,0,108,0,108,0,101,0,116,0,67,0,111,0,108,0,111,0,114,0,0,0
-shotnum db "a",0,"n",0,"t",0,"_",0,"0",0,"1",00h,00h,00h
+antBulletColor db 97,0,110,0,116,0,95,0,66,0,117,0,108,0,108,0,101,0,116,0,67,0,111,0,108,0,111,0,114,0,0,0
+; L"ant_BulletExSet"
+antBulletExSet db 97,0,110,0,116,0,95,0,66,0,117,0,108,0,108,0,101,0,116,0,69,0,120,0,83,0,101,0,116,0,0,0
+; L"ant_BulletFireHeight"
+antBulletHeight db 97,0,110,0,116,0,95,0,66,0,117,0,108,0,108,0,101,0,116,0,70,0,105,0,114,0,101,0,72,0,101,0,105,0,103,0,104,0,116,0,0,0
 
 .code
 edf5BDF30 proc
@@ -12,49 +17,11 @@ edf5BDF30 ENDP
 
 ASMAntAcidColor proc
 
-mov dword ptr [rbx+12D8h], 360 ; AmmoAlive
-;mov dword ptr [rbx+1320h], 0 ;height
-mov dword ptr [rbx+12F4h], 3E800000h ; AmmoSize
-;mov dword ptr [rbx+12E8h], 41F00000h ; AmmoExplosion
-;mov byte ptr [rbx+12ECh], 1 ; AmmoIsPenetration
-; rbx+12D4h AmmoSpeed
-; rbx+12DCh AmmoDamage
-
-lea rdx, color
+lea rdx, antBulletAlive
 mov rcx, rsi
 call edf5BDF30 ; read sgo node
 cmp eax, -1
-je ShotNumBlock ; if node does not exist, jump to shot count
-mov r8, [rsi]
-cdqe
-movsxd rdx, dword ptr [r8+0Ch]
-lea rcx, [rax+rax*2]
-lea rax, [r8+rdx]
-lea rcx, [rax+rcx*4]
-; change ammo color
-movsxd rax, dword ptr [rcx+8]
-movss xmm0, dword ptr [rax+rcx+8]
-movss dword ptr [rbx+1310h], xmm0
-movsxd rax, dword ptr [rcx+8]
-movss xmm0, dword ptr [rax+rcx+20]
-movss dword ptr [rbx+1314h], xmm0
-
-mulss xmm0, dword ptr [rbx+12D4h]
-movss dword ptr [rbx+12D4h], xmm0
-
-movsxd rax, dword ptr [rcx+8]
-movss xmm0, dword ptr [rax+rcx+32]
-movss dword ptr [rbx+1318h], xmm0
-movsxd rax, dword ptr [rcx+8]
-movss xmm0, dword ptr [rax+rcx+44]
-movss dword ptr [rbx+131Ch], xmm0
-
-ShotNumBlock:
-lea rdx, shotnum
-mov rcx, rsi
-call edf5BDF30 ; read sgo node
-cmp eax, -1
-je OriginalEndBlock ; if node does not exist, jump to end
+je AmmoColorBlock ; if node does not exist, jump
 ; read int in node 
 mov r8, [rsi]
 cdqe
@@ -62,8 +29,100 @@ movsxd rdx, dword ptr [r8+0Ch]
 lea rcx, [rax+rax*2]
 lea rax, [r8+rdx]
 mov ecx, [rax+rcx*4+8]
-imul ecx, [rbx+13B0h] ; old count * this int
-mov [rbx+13B0h], ecx ; rbx+13B0h is shot count
+mov [rbx+12D8h], ecx ; AmmoAlive
+
+AmmoColorBlock:
+lea rdx, antBulletColor
+mov rcx, rsi
+call edf5BDF30 ; read sgo node
+cmp eax, -1
+je AmmoExSetBlock ; if node does not exist, jump
+; read ptr in node
+mov r8, [rsi]
+cdqe
+movsxd rdx, dword ptr [r8+0Ch]
+lea rcx, [rax+rax*2]
+lea rax, [r8+rdx]
+lea rcx, [rax+rcx*4] ; store ptr address
+; change ammo color
+movsxd rax, dword ptr [rcx+8]
+movss xmm0, dword ptr [rax+rcx+8]
+movss dword ptr [rbx+1310h], xmm0 ;r
+movsxd rax, dword ptr [rcx+8]
+movss xmm0, dword ptr [rax+rcx+20]
+movss dword ptr [rbx+1314h], xmm0 ;g
+movsxd rax, dword ptr [rcx+8]
+movss xmm0, dword ptr [rax+rcx+32]
+movss dword ptr [rbx+1318h], xmm0 ;b
+movsxd rax, dword ptr [rcx+8]
+movss xmm0, dword ptr [rax+rcx+44]
+movss dword ptr [rbx+131Ch], xmm0 ;a
+
+AmmoExSetBlock:
+lea rdx, antBulletExSet
+mov rcx, rsi
+call edf5BDF30 ; read sgo node
+cmp eax, -1
+je ShotHeightBlock ; if node does not exist, jump
+; read ptr in node
+mov r8, [rsi]
+cdqe
+movsxd rdx, dword ptr [r8+0Ch]
+lea rcx, [rax+rax*2]
+lea rax, [r8+rdx]
+lea rcx, [rax+rcx*4] ; store ptr address
+; change shot count
+movsxd rax, dword ptr [rcx+8]
+mov eax, [rax+rcx+8]
+imul eax, [rbx+13B0h] ; old * this
+mov [rbx+13B0h], eax
+; change ammo speed
+movsxd rax, dword ptr [rcx+8]
+movss xmm0, dword ptr [rax+rcx+20]
+mulss xmm0, dword ptr [rbx+12D4h] ; old * this
+movss dword ptr [rbx+12D4h], xmm0
+; change ammo damage
+movsxd rax, dword ptr [rcx+8]
+movss xmm0, dword ptr [rax+rcx+32]
+mulss xmm0, dword ptr [rbx+12DCh] ; old * this
+movss dword ptr [rbx+12DCh], xmm0
+; change ammo explosion range
+movsxd rax, dword ptr [rcx+8]
+movss xmm0, dword ptr [rax+rcx+44]
+movss dword ptr [rbx+12E8h], xmm0
+; change ammo damage
+movsxd rax, dword ptr [rcx+8]
+lea rax, [rax+rcx+48] ; store address
+cmp dword ptr [rax+8],0
+setne al
+mov byte ptr [rbx+12ECh], al
+; change ammo size
+movsxd rax, dword ptr [rcx+8]
+movss xmm0, dword ptr [rax+rcx+68]
+movss dword ptr [rbx+12F4h], xmm0
+; change ammo hit size
+movsxd rax, dword ptr [rcx+8]
+movss xmm0, dword ptr [rax+rcx+80]
+movss dword ptr [rbx+12F8h], xmm0
+; change ammo hit impulse
+movsxd rax, dword ptr [rcx+8]
+movss xmm0, dword ptr [rax+rcx+92]
+movss dword ptr [rbx+12FCh], xmm0
+
+ShotHeightBlock:
+lea rdx, antBulletHeight
+mov rcx, rsi
+call edf5BDF30 ; read sgo node
+cmp eax, -1
+je OriginalEndBlock ; if node does not exist, jump
+; read float in node 
+mov r8, [rsi]
+cdqe
+movsxd rdx, dword ptr [r8+0Ch]
+lea rcx, [rax+rax*2]
+lea rax, [r8+rdx]
+movss xmm0, dword ptr [rax+rcx*4+8]
+movss dword ptr [rbx+1320h], xmm0 ; shot height
 
 OriginalEndBlock:
 add rsp, 0A8h
